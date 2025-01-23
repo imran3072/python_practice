fd554655813d84adb55c195d4c1afb7bca7f1dff
imran3072 <imranhossain120465@gmail.com>
2025-01-23 23:00:28 +0600

    first commit

---
+++ first.py
@@ -0,0 +1,32 @@
+# a = 2
+# b = 'shuvo'
+# print(f"integer: {a}")
+# print(f"string: {b}")
+
+a = 'Imran'
+b = 'Saoda'
+
+# print(a + ' love ' + b)
+c = 10
+d = '10'
+# print(c+int(d))
+
+e = 'imranlovesaoda'
+# print(len(e))
+f = e[5:]
+# print(f)
+
+person = {
+    'name': 'shuvo',
+    'age': 24,
+    'email': 'shuvo@gmail.com'
+}
+
+# for key in person:
+    # print(key, ':', person[key])
+    
+for i in range(1, 10):
+    if i % 2 == 0:
+        print(i)
+        
+
