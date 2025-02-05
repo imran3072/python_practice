2b80bcb423dc1666f8aca9f9e04e044d370b6895
imran3072 <imranhossain120465@gmail.com>
2025-02-05 22:25:44 +0600

    imran

--- third.py
+++ third.py
@@ -1,1 +1,57 @@
-print('third')
+# this is my personal im=nformation:
+#print ("01798173527")
+#print(10.5)
+#print("MD Imran Hossain")
+
+
+#for single and multiline comments
+#Print("MD Imran Hossain")
+'''
+this 
+is my 
+personal
+information 
+'''
+#use for \n,\t new line and genarate
+
+#print( "\" Md Imran Hossain\" \n 01798173527" )
+
+# python variables:
+'''
+name = "Md. Ridoy Hossain"
+age = 24
+cgpa = 3.96
+print("My name is " +name )
+print("i am ",age,"years old")
+print("My cgpa is ",cgpa,"")
+'''
+
+#print("MD "+ "Imran " + "Hossain " + "Naim")
+
+'''x = int("10")
+y = 5
+z = int("2")
+result = x + y + z
+print(result)
+'''
+
+'''x = "10"
+num = int(x)
+y = 5
+z = "2"
+num1 = int(z)
+
+result = y + num + num1
+print(result)'''
+
+x = int(input("Enter your number:")) 
+y = int(input("Enter your number:"))
+z = int(input("Enter your number:"))
+
+result = x + y + z
+print(result)
+
+if result % 2 == 0:
+    print("the result is even")
+else:
+    print("the result is odd")
